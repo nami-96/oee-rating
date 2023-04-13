a6ef1f79753d23937e50815db83968af57c29c4f
Nami96 <nabilnazmi96@gmail.com>
2023-04-13 18:35:24 +0800

    add cp script for more files

---
+++ cp-logout.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/bash
+
+# Enter the source and destination directory paths below
+source_dir="C:/Users/nabil/Desktop/PHP-Projects/oee-rating"
+dest_dir="C:/xampp2/htdocs/oee-rating"
+
+# Enter the filename of the file you want to copy
+file_name="logout.php"
+
+# Use the 'cp' command to copy the file from source directory to destination directory
+cp "${source_dir}/${file_name}" "${dest_dir}/${file_name}"
+
+# Print a success message
+echo "File ${file_name} has been successfully copied from ${source_dir} to ${dest_dir}."
+
--- cp-oee.sh
+++ cp-oee.sh
@@ -2,10 +2,10 @@
 
 # Enter the source and destination directory paths below
 source_dir="C:/Users/nabil/Desktop/PHP-Projects/oee-rating"
-dest_dir="C:/xampp2/htdocs"
+dest_dir="C:/xampp2/htdocs/oee-rating"
 
 # Enter the filename of the file you want to copy
-file_name="OEE_Rating.php"
+file_name="oee.php"
 
 # Use the 'cp' command to copy the file from source directory to destination directory
 cp "${source_dir}/${file_name}" "${dest_dir}/${file_name}"
