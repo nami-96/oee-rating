4ea48d0c48495c5a74979d7245abc6fa1b8778b8
Nami96 <nabilnazmi96@gmail.com>
2023-04-13 18:36:03 +0800

    chnge user n pass

--- connection.php
+++ connection.php
@@ -1,1 +1,11 @@
 <?php
+
+$dbhost = "localhost";
+$dbuser = "nami96";
+$dbpass = "roxas1996";
+$dbname = "login_sample_db";
+
+if (!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
+{
+    die("failed to connect!");
+}
