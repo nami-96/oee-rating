487d43d7ba990565a198cdd684818067533878b0
Nami96 <nabilnazmi96@gmail.com>
2023-04-19 03:39:19 +0800

    change db user name

--- htdocs/connection.php
+++ htdocs/connection.php
@@ -1,11 +1,11 @@
 <?php
 
 $dbhost = "localhost";
-$dbuser = "nami96";
+$dbuser = "root";
 $dbpass = "roxas1996";
 $dbname = "login_sample_db";
 
-if (!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
+if (!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname)) //establish connection to database
 {
     die("failed to connect!");
 }
