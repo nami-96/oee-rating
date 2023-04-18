cb0bf4dc1c4053b83aa53bc00f70c4deef1de06a
Nami96 <nabilnazmi96@gmail.com>
2023-04-19 03:39:36 +0800

    add user id length function

--- htdocs/functions.php
+++ htdocs/functions.php
@@ -19,3 +19,21 @@ function check_login($con) {
     header("location: login.php");
     die;
 }
+
+function random_num($length)
+    {
+        $text = "";
+        if($length < 5)
+        {
+            $length = 5;
+        }
+
+        $len  = rand(4,$length);
+
+        for ($i=0; $i < $len; $i++) {
+
+            $text .= rand(0,9);
+        }
+
+        return $text;
+    }
