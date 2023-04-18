74cd1b64dffd7193980cf6e44169e0d8715b5e61
Nami96 <nabilnazmi96@gmail.com>
2023-04-19 03:39:02 +0800

    edit cp script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash
+
+# Use the 'cp' command to copy the file from source directory to destination directory
+rm -rf C:/xampp2/htdocs
+cp -r C:/Users/nabil/Desktop/PHP-Projects/oee-rating/htdocs C:/xampp2/htdocs
+
+# Print a success message
+echo "Deployed to server."
