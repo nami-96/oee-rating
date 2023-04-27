b865091eb405f41ea34f27b693ee5b7ae708a6c1
Nami96 <nabilnazmi96@gmail.com>
2023-04-27 17:39:24 +0800

    establish $con on separate line

--- htdocs/connection.php
+++ htdocs/connection.php
@@ -4,8 +4,9 @@ $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "roxas1996";
 $dbname = "login_sample_db";
+$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
 
-if (!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname)) //establish connection to database
+if (!$con) //establish connection to database
 {
     die("failed to connect!");
 }
